b48d5dbd9f048a4d6b9280368cb53578391d1e3b
jainipanikar <jainipanikar@gmail.com>
2022-10-09 13:20:26 -0400

    first commit

---
+++ radius.java
@@ -0,0 +1,21 @@
+package circle;
+
+import java.util.Scanner;
+
+public class radius {
+	
+public static void main(String[] args) {
+    	
+    	Scanner in = new Scanner(System.in);
+		   
+		  System.out.print("Please enter a radius: ");
+		  double radius = in.nextDouble();
+
+        double perimeter = 2 * Math.PI * radius;
+        double area = Math.PI * radius * radius;
+
+        System.out.println("Perimeter is = " + perimeter);
+        System.out.println("Area is = " + area);
+    }
+
+}
